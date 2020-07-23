ab02323f5d651cdc8b807b6dfdafcb3adda1022e
Prabhat Sharma <hi.prabhat@gmail.com>
2020-07-22 17:14:51 -0700

    changed cluster name

--- scriptv1.sh
+++ scriptv1.sh
@@ -8,7 +8,7 @@ export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/doc
 
 CLUSTER_RAND=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 
-export AWS_CLUSTER_NAME=eksworkshop-$CLUSTER_RAND
+export AWS_CLUSTER_NAME=kubeflow
 
 aws configure set default.region ${AWS_REGION}
 #aws configure set aws_output json
